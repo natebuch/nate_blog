39c45e7f6d02879035a26ac7192ca76f82b6f822
natebuch <nate.buch@gmail.com>
2018-08-23 20:55:54 -0500

    Add user validations"

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { minimum: 10, maximum: 50}
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
-
+  validates :user_id, presence: true
 
 end
