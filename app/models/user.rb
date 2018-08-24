424880dbc89a95d2ab17e48de31655cff42a2944
natebuch <nate.buch@gmail.com>
2018-08-23 22:15:10 -0500

    added secure password storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+
   before_save { self.email = email.downcase }
 
   validates :username, presence: true, 
@@ -12,4 +13,7 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false},
     length: { maximum: 105},
     format: { with: VALID_EMAIL_REGEX }
+
+   has_secure_password
+
 end
