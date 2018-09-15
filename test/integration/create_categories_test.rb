1983d2df20ca10353288ecb4cf646754cb058e36
natebuch <nate.buch@gmail.com>
2018-09-15 14:51:00 -0500

    Add categories dropdown to navbar

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,20 +1,33 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
 
 	def setup
-
-	  @category = Category.create(name: "books")
-	  @category2 = Category.create(name: "programming")
+    @user = User.create(username: "tester", email: "tester@email.com", password: "password", admin: true)
 	end
 
-	test "should show categories listing" do
-	  get categories_path
-	  assert_template 'categories/index'
-	  assert_select "a[href=?]", category_path(@category), text: @category.name	
-	  assert_select "a[href=?]", category_path(@category2), text: @category2.name
-	end
+	test "get new category form and create category" do
+	  sign_in_as(@user, "password")
+	  get new_category_path
+	  assert_template 'categories/new'
+	  assert_difference 'Category.count', 1 do
+	    post categories_path, params: {category: {name: "bookss"}}
+	    follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match "bookss", response.body
+  end
 
-
-
+  test "invalid category submission results in failure" do
+  	sign_in_as(@user, "password")
+  	get new_category_path
+	  assert_template 'categories/new'
+	  assert_no_difference 'Category.count' do
+	    post categories_path, params: {category: {name: " "}}
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.alert-title'
+    assert_select 'div.alert-body'
+  end
+		
 end
--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,27 +1,19 @@
 require 'test_helper'
 
-class ListCategoriesTest < ActionDispatch::IntegrationTest
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
-	test "get new category form and create category" do
-	  get new_category_path
-	  assert_template 'categories/new'
-	  assert_difference 'Category.count', 1 do
-	    post categories_path, params: {category: {name: "sports"}}
-	    follow_redirect!
-    end
-    assert_template 'categories/index'
-    assert_match "sports", response.body
-  end
-
-  test "invalid category submission results in failure" do
-  	get new_category_path
-	  assert_template 'categories/new'
-	  assert_difference 'Category.count' do
-	    post categories_path, params: {category: {name: " "}}
-    end
-    assert_template 'categories/new'
-    assert_select 'h2.panel-title'
-    assert_select 'div.panel-body'
-  end
-		
+	def setup
+	  @category = Category.create(name: "books")
+	  @category2 = Category.create(name: "programming")
 	end
+
+	test "should show categories listing" do
+	  get categories_path
+	  assert_template 'categories/index'
+	  assert_select "a[href=?]", category_path(@category), text: @category.name	
+	  assert_select "a[href=?]", category_path(@category2), text: @category2.name
+	end
+
+
+
+end
